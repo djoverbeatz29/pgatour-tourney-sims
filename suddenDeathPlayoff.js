c473adabd9f5bffc211f76bea93f4ed640f5ea44
jequezinho <jroverby92@gmail.com>
2021-01-24 23:23:34 -0600

    Added suddenDeathPlayoff() function to handle tiebreaks, in cas of multiple winners at end of a round; have not yet integrated into winProbSimulator()

---
+++ suddenDeathPlayoff.js
@@ -0,0 +1,13 @@
+function suddenDeathPlayoff(playersArray, holeArray) {
+    let playersCopy=JSON.parse(JSON.stringify(playersArray));
+    while (playersCopy.length>1) {
+        let lead=50;
+        playersCopy.forEach(player=>{
+            player.score=0;
+            playerRound(player, [holeArray]);
+            if (player.score<lead) lead=player.score;
+        });
+        playersCopy=playersCopy.filter(player=>player.score===lead);
+    }
+    return playersCopy[0];
+}
