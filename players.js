8aaeda3186f2afbd9c972da3dfbb744c4399acdd
jequezinho <jroverby92@gmail.com>
2021-01-24 21:48:12 -0600

    Turned players variable from Object to Array and adjusted relevant functions to handle the array

--- players.js
+++ players.js
@@ -1,14 +1,14 @@
-let players = {
-    "Max Homa": {SGT: 0.279, score: -15},
-    "Si Woo Kim": {SGT: 0.267, score: -15},
-    "Tony Finau": {SGT: 1.243, score: -15},
-    "Richy Werenski": {SGT: 0.484, score: -14},
-    "Russell Knox": {SGT: 0.030, score: -13},
-    "Brian Harman": {SGT: 0.767, score: -13},
-    "Emiliano Grillo": {SGT: -0.182, score: -13},
-    "Cameron Davis": {SGT: 0.560, score: -12},
-    "Rory Sabbatini": {SGT: 0.071, score: -12},
-    "Chase Seiffert": {SGT: 0.196, score: -12},
-    "Francesco Molinari": {SGT: 0.038, score: -12},
-    "Doug Ghim": {SGT: -0.520, score: -12}
-}
+let players = [
+   {name: "Max Homa", SGT: 0.279, score: -15},
+   {name: "Si Woo Kim", SGT: 0.267, score: -15},
+   {name: "Tony Finau", SGT: 1.243, score: -15},
+   {name: "Richy Werenski", SGT: 0.484, score: -14},
+   {name: "Russell Knox", SGT: 0.030, score: -13},
+   {name: "Brian Harman", SGT: 0.767, score: -13},
+   {name: "Emiliano Grillo", SGT: -0.182, score: -13},
+   {name: "Cameron Davis", SGT: 0.560, score: -12},
+   {name: "Rory Sabbatini", SGT: 0.071, score: -12},
+   {name: "Chase Seiffert", SGT: 0.196, score: -12},
+   {name: "Francesco Molinari", SGT: 0.038, score: -12},
+   {name: "Doug Ghim", SGT: -0.520, score: -12}
+];
--- simulateRound.js
+++ simulateRound.js
@@ -1,6 +1,6 @@
 function simulateRound(playersArray, courseObj) {
     playersArray.forEach(player=>playerRound(player, courseObj));
     const bestScore = Math.max(...playersArray.map(player=>player.score));
-    const winners = players.filter(player=>player.score=bestScore);
+    const winners = playersArray.filter(player=>player.score=bestScore);
     return winners;
   }
